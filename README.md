4b5cb77e11272af35aa72c9baed007d34739697c
Titiplex <107863779+Titiplex@users.noreply.github.com>
2026-04-17 10:43:22 -0400

    Initial commit, electron basic setup following tutorial

---
+++ README.md
@@ -0,0 +1,10 @@
+Create a distributable
+
+````shell
+npm run make
+````
+
+Publish 
+````shell
+npm run publish
+````
---
+++ main.js
@@ -0,0 +1,34 @@
+const { app, BrowserWindow, ipcMain } = require('electron/main')
+const { updateElectronApp } = require('update-electron-app')
+updateElectronApp()
+const path = require('node:path')
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js')
+        }
+    })
+
+    win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+    createWindow()
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow()
+        }
+    })
+
+    ipcMain.handle('ping', () => 'pong')
+})
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
